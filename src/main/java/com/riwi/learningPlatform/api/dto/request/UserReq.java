8bb3520dec2567807bd3e04fffbbab8c5865742d
mtatiana115 <mtatiana115@gmail.com>
2024-06-03 17:26:06 -0500

    fix

--- src/main/java/com/riwi/learningPlatform/api/dto/request/UserReq.java
+++ src/main/java/com/riwi/learningPlatform/api/dto/request/UserReq.java
@@ -2,9 +2,6 @@ package com.riwi.learningPlatform.api.dto.request;
 
 import com.riwi.learningPlatform.util.enums.RoleUser;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
